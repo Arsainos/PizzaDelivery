9fa7ad69addee5fff8e880b023765e39ca078912
Belyavtsev Artyem <arsainos@gmail.com>
2020-01-29 01:53:10 +0300

    Cart button and nav panel

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem.js';
+
+const navigationItems = (props) =>(
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" exact>Main</NavigationItem>
+        <NavigationItem link="/pizzas">Pizzas</NavigationItem>
+        <NavigationItem link="/pizza_builder">Build you Pizza</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,21 +5,22 @@ import classes from './Toolbar.module.css';
 
 // import components
 import Button from '../../UI/Button/Button.js';
+import NavigationItems from '../NavigationItems/NavigationItems.js';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div className={classes.Ribon}/>
-        <div>
+        <div className={classes.TopPanel}>
             <div>
                 <h1>Logo</h1>
             </div>
-            <div>
+                <div>
                 <Button><h1>Cart</h1></Button>
             </div>
-        </div>
-        <div>
+        </div>    
+        <div className={classes.NavPanel}>
             <nav>
-                
+                <NavigationItems />
             </nav>
         </div>
     </header>
