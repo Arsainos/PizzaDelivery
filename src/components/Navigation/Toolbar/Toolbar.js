e4f302a38c18138f4f392385ad04cfb25922ca2d
Belyavtsev Artyem <arsainos@gmail.com>
2020-01-29 01:24:59 +0300

    add button to toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,15 +3,18 @@ import React from 'react';
 // import styles
 import classes from './Toolbar.module.css';
 
+// import components
+import Button from '../../UI/Button/Button.js';
+
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div className={classes.Ribon}/>
         <div>
             <h1>Logo</h1>
         </div>
-        <nav>
-            
-        </nav>
+        <div>
+            <Button><h1>Cart</h1></Button>
+        </div>
     </header>
 );
 
