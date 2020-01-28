b30ebbf4b6f553aa3372c15afc73395eac3300aa
Belyavtsev Artyem <arsainos@gmail.com>
2020-01-29 01:10:38 +0300

    add lazy loading for main page container in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+
+// import styles
 import classes from './App.module.css';
 
+// import components
 import Layout from './components/Layout/Layout.js';
+
+// import hocs
+import asyncComponent from './hocs/asyncComponent/asyncComponent.js';
+
+// import async containers
+const asyncMainPage = asyncComponent(() => {
+  return import ('./containers/MainPage/MainPage.js');
+});
 
 function App() {
   return (
