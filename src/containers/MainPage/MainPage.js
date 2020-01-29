defcccffd640215efa0bc0b550a50d950fba0ec1
Belyavtsev Artyem <arsainos@gmail.com>
2020-01-29 17:06:52 +0300

    add carousel

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -6,12 +6,24 @@ import classes from './MainPage.module.css';
 // import hocs 
 import AUX from '../../hocs/Axuliary.js';
 
+// import components
+import Carousel from '../../components/UI/Carousel/Carousel.js';
+
+// import images
+import carousel1 from '../../assets/Carousel/carousel1.jpg';
+import carousel2 from '../../assets/Carousel/carousel2.jpg';
+import carousel3 from '../../assets/Carousel/carousel3.jpg';
+
 const mainPage = (props) => {
 
     return(
         <AUX>
             <div>
-                <h1>CAROUSEL</h1>
+                <Carousel>
+                    <div><img src={carousel1} alt="carousel yamu pizza"></img></div>
+                    <div><img src={carousel2} alt="carousel yamu pizza"></img></div>
+                    <div><img src={carousel3} alt="carousel yamu pizza"></img></div>
+                </Carousel>
             </div>
             <div>
                 <h1>pizza list</h1>
