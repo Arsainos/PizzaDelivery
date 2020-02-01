1179d707b2ad75a29ba300e8b1a2f9b426aad28a
Belyavtsev Artyem <arsainos@gmail.com>
2020-02-01 17:06:12 +0300

    add promocard component

--- src/components/UI/Cards/PromoCard/PromoCard.js
+++ src/components/UI/Cards/PromoCard/PromoCard.js
@@ -1,1 +1,23 @@
 import React from 'react';
+
+//import styles
+import classes from './PromoCard.module.css';
+
+//import image
+import promoImg from '../../../../assets/mocks/promo.jpeg';
+
+const PromoCard = (props) => {
+    return(
+        <div className={classes.Promo}>
+            <div className={classes.PromoImage}>
+                <img className={classes.PromoImageImg} src={promoImg} alt={'promo img'}/>
+            </div>          
+            <h3 className={classes.PromoTitle}>{props.title}</h3>
+            <div className={classes.PromoDescription}>
+                {props.description}
+            </div>
+        </div>
+    )
+}
+
+export default PromoCard;
