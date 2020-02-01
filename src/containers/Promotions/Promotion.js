5d776d01518a2975144f48cd53a035a4c26ec050
Belyavtsev Artyem <arsainos@gmail.com>
2020-02-01 17:30:52 +0300

    change description text to lorem ipsum

--- src/containers/Promotions/Promotion.js
+++ src/containers/Promotions/Promotion.js
@@ -6,9 +6,9 @@ import PromoCard from '../../components/UI/Cards/PromoCard/PromoCard.js';
 
 const Promotions = (props) => {
     const promoArray = [
-        {title: '2x2 Coca-cola', description:'Buy pack of two coca-cola and get another two for three!', imageSrc:''},
-        {title: 'Special price for bundles!', description:'Buy any bundles and get -10% discount', imageSrc:''},
-        {title: 'Night watch', description:'Order pizza in night time (from 24:00 to 06:00) and get -50% discount'},
+        {title: '2x2 Coca-cola', description: 'Veniam dolore ullamco cupidatat elit sint Lorem deserunt mollit non ipsum qui anim adipisicing. Commodo tempor magna velit est dolor incididunt velit deserunt veniam esse. Sint adipisicing nostrud ipsum fugiat do id deserunt velit amet dolor est cupidatat sit veniam.', imageSrc:''},
+        {title: 'Special price for bundles!', description:'Veniam aliqua labore amet duis excepteur reprehenderit quis occaecat. Excepteur mollit et amet voluptate exercitation aute dolor dolor consectetur cillum incididunt quis enim. Sint velit excepteur quis anim minim enim et incididunt.', imageSrc:''},
+        {title: 'Night watch', description:'Nisi excepteur enim ea eiusmod reprehenderit ea nisi ex officia occaecat reprehenderit eiusmod velit sint. Id proident aute in veniam pariatur. Duis pariatur adipisicing tempor ipsum veniam consectetur velit duis. Est ad tempor sunt cillum nostrud nulla proident ipsum consectetur proident ipsum dolor. Laboris cillum labore dolor cillum aliquip eu aliquip voluptate qui.'},
     ]
     
     let promoCards = promoArray.map((it, i) => {
