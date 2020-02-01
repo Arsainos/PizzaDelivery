cef6d6dc731981b4ca31a80c51fe48e3d794c0eb
Belyavtsev Artyem <arsainos@gmail.com>
2020-02-02 00:18:57 +0300

    card order

--- src/components/UI/Cards/CardOrder/CardOrder.js
+++ src/components/UI/Cards/CardOrder/CardOrder.js
@@ -5,9 +5,74 @@ import classes from './CardOrder.module.css';
 
 // import image
 import pizzaImage from '../../../../assets/mocks/pizzaMock.jpg';
+import card from '../Card/Card';
 
 const CardOrder = (props) => {
-    const [cardOrderState, cardOrderUpdate] = useState({summary:`Traditional Dough, Small size`, size:`Small`})
+    const [cardOrderState, cardOrderUpdate] = useState({summary:`Traditional Dough, Small Size`, size:{name:`small`, price:0} , dough:{name:`traditional`, price:0} , price:props.price, additions:[]});
+
+    const onRadioChangeHandler = (type, value) => {
+        const basePrice = Number.parseFloat(props.price);
+        const doughPrice = type === 'dough' ? value.price : cardOrderState.dough.price;
+        const sizePrice = type === 'size' ? value.price : cardOrderState.size.price;
+        let additionsPrice = 0;
+        if(cardOrderState.additions.length > 0) {
+            cardOrderState.additions.forEach((addition) => {
+                additionsPrice = additionsPrice + addition.price;
+            });
+        };
+        const summaryPrice = basePrice + doughPrice + sizePrice + additionsPrice;
+
+        const summary = `${type === 'dough' ? value.name[0].toUpperCase() + value.name.slice(1, value.length) : cardOrderState.dough.name[0].toUpperCase() + cardOrderState.dough.name.slice(1, cardOrderState.dough.name.length)} Dough,
+            ${type === 'size' ? value.name[0].toUpperCase()+value.name.slice(1, value.name.length) : cardOrderState.size.name[0].toUpperCase()+cardOrderState.size.name.slice(1, cardOrderState.size.name.length)} Size`;
+
+        cardOrderUpdate({...cardOrderState, summary:summary, [type]:value, price:summaryPrice});  
+    };
+
+    const onCheckboxChangeHandler = (value) => {
+        let updatedAdditions = [...cardOrderState.additions];
+        if(updatedAdditions.some((it) => it.name === value.name)) {
+            updatedAdditions = updatedAdditions.filter((addition) => addition.name !== value.name);
+        } else {
+            updatedAdditions.push(value);
+        } 
+        const basePrice = Number.parseFloat(props.price);
+        const doughPrice = cardOrderState.dough.price;
+        const sizePrice = cardOrderState.size.price;
+        let additionsPrice = 0; 
+        if(updatedAdditions.length > 0) {
+            updatedAdditions.forEach((addition) => {
+                additionsPrice = additionsPrice + addition.price;
+            })
+        };
+        const summaryPrice = basePrice + doughPrice + sizePrice + additionsPrice;
+
+        cardOrderUpdate({...cardOrderState, price:summaryPrice, additions: updatedAdditions});
+    };
+
+    const generateCheckboxes = (array, inputType, type, handler) => {
+        return array.map((it,i) => {
+            return (
+                <li className={classes.Checkbox} key={it.name+'_'+i}>
+                <input 
+                    className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')}
+                    type={inputType}
+                    id={type+'-'+it.name}
+                    name={type}
+                    value={it.name}
+                    checked={inputType === `radio` ? {...cardOrderState[type]}.name === it.name : cardOrderState.additions.some((addition) => addition.name === it.name)}
+                    onChange={inputType === `radio` ? () => handler(type,it) : () => handler(it)}
+                />
+                <label className={classes.CheckboxLabel} htmlFor={type+'-'+it.name}>
+                    {it.name[0].toUpperCase() + it.name.slice(1, it.name.length)}
+                </label>
+            </li>
+            )
+        })
+    }
+
+    const sizeCheckboxes = generateCheckboxes([{name:`small`, price:0},{name:`medium`, price:3},{name:`large`, price:8}],`radio`,`size`,onRadioChangeHandler);
+    const doughCheckboxes = generateCheckboxes([{name:`traditional`, price:0},{name:`thin`, price:4}],`radio`,`dough`,onRadioChangeHandler);
+    const toppingCheckboxes = generateCheckboxes([{name:`mushrooms`, price:2}, {name:`mozzarella`, price:3},{name:`jalapeno`,price:3}],`checkbox`,`toppings`,onCheckboxChangeHandler);
 
     return(
         <div className={classes.CardOrder} >
@@ -21,51 +86,22 @@ const CardOrder = (props) => {
                 <div className={classes.OptionsContainer} >
                     <h3 className={classes.OptionsHeading} >Size</h3>
                     <ul className={classes.OptionsList} >
-                        <li className={classes.Checkbox} >
-                            <input className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')} type="radio" id="size-s" name="size"  value="1" checked={true}/>
-                            <label className={classes.CheckboxLabel} htmlFor="size-s" >Small</label>
-                        </li>
-                        <li className={classes.Checkbox} >
-                            <input className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')} type="radio" id="size-m" name="size"  value="1" />
-                            <label className={classes.CheckboxLabel} htmlFor="size-m" >Medium</label>
-                        </li>
-                        <li className={classes.Checkbox} >
-                            <input className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')} type="radio" id="size-l" name="size"  value="1" />
-                            <label className={classes.CheckboxLabel} htmlFor="size-l" >Large</label>
-                        </li>
+                        {sizeCheckboxes}
                     </ul>
                 </div>
                 <div className={classes.OptionsContainer} >
                     <h3 className={classes.OptionsHeading} >Dough</h3>
                     <ul className={classes.OptionsList} >
-                        <li className={classes.Checkbox} >
-                            <input className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')} type="radio" id="dough-traditional" name="dough"  value="1" checked={true} />
-                            <label className={classes.CheckboxLabel} htmlFor="dough-traditional" >Traditional</label>
-                        </li>
-                        <li className={classes.Checkbox} >
-                            <input className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')} type="radio" id="dough-thin" name="dough"  value="1" />
-                            <label className={classes.CheckboxLabel} htmlFor="dough-thin" >Thin</label>
-                        </li>
+                        {doughCheckboxes}
                     </ul>
                 </div>
                 <div className={classes.OptionsContainer} >
                     <h3 className={classes.OptionsHeading} >Toppings</h3>
                     <ul className={classes.OptionsList} >
-                        <li className={classes.Checkbox} >
-                            <input className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')} type="checkbox" id="toppings-mushrooms" name="toppings"  value="1" />
-                            <label className={classes.CheckboxLabel} htmlFor="toppings-mushrooms" >Mushrooms</label>
-                        </li>
-                        <li className={classes.Checkbox} >
-                            <input className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')} type="checkbox" id="toppings-mozzarella" name="toppings"  value="1" />
-                            <label className={classes.CheckboxLabel} htmlFor="toppings-mozzarella" >Mozzarella</label>
-                        </li>
-                        <li className={classes.Checkbox} >
-                            <input className={[classes.InputCheckbox, classes.VisuallyHidden].join(' ')} type="checkbox" id="toppings-jalapeno" name="toppings"  value="1" />
-                            <label className={classes.CheckboxLabel} htmlFor="toppings-jalapeno" >Jalapeno</label>
-                        </li>
+                        {toppingCheckboxes}
                     </ul>
                 </div>
-                <button className={classes.AddButton}  onClick={props.continueClick}>{`Add to Cart for `+props.price}</button>
+                <button className={classes.AddButton}  onClick={() => props.continueClick(props, cardOrderState)}>{`Add to Cart for `+cardOrderState.price}</button>
             </div>
         </div>
     )
