e6913bcb1397186bc8d63f65874273963994d83a
Belyavtsev Artyem <arsainos@gmail.com>
2020-02-02 14:34:21 +0300

    add configuration for redux stores

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import { BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+
+// for react-toolkit
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers(
+    {
+        ingredient:null,
+        auth:null,
+        order:null,
+        food:null
+    }
+);
+
+const store = createStore(rootReducer,composeEnhancers());
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
