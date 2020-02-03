8d08d90612966b01892b4f74091918d57f020762
Belyavtsev Artyem <arsainos@gmail.com>
2020-02-03 23:09:46 +0300

    get rid off unused import

--- src/components/UI/Carousel/Carousel.js
+++ src/components/UI/Carousel/Carousel.js
@@ -3,7 +3,6 @@ import React, {Component} from 'react';
 // import styles
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import classes from './Carousel.model.css';
 
 // import components
 import Slider from "react-slick";
